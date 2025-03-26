d820477da61032786f7eda680e42dc082fdbae83
maxim kim <maximk@umich.edu>
2025-03-26 01:12:52 -0400

    more setup

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,42 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void error_callback(int error, const char *description) {
+void errorCallback(int error, const char *description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
+void keyCallback(GLFWwindow *window, int key, int scancode, int action,
+                 int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        printf("inside\n");
+    }
+    printf("happened\n");
+}
+
 int main() {
-    glfwSetErrorCallback(error_callback);
+    glfwSetErrorCallback(errorCallback);
     if (!glfwInit()) {
         printf("glfwinit error\n");
         exit(1);
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     GLFWwindow *window = glfwCreateWindow(800, 600, "GLFW Window", NULL, NULL);
     if (!window) {
         printf("window creation error\n");
         exit(1);
     }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glfwSetKeyCallback(window, keyCallback);
     while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+        // glfwPollEvents();
+        glfwSwapBuffers(window);
     }
+
     glfwDestroyWindow(window);
     glfwTerminate();
 }

d12878961a3dde305aabb527f1ef5b54c2f7525f
maxim kim <maximk@umich.edu>
2025-03-27 20:21:43 -0400

    switching to sdl for easier 2d graphics

--- src/cool.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/cool.hpp"
-#include <iostream>
-
-void evil(int a) {
-    printf("hello there\n");
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,91 @@
-#define GLFW_INCLUDE_NONE
-#include "../include/cool.hpp"
-#include "../include/glad/glad.h"
-#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_scancode.h>
 #include <iostream>
 
-void errorCallback(int error, const char *description) {
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-void keyCallback(GLFWwindow *window, int key, int scancode, int action,
-                 int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
-}
-
-void update() {
-}
-
-void draw() {
-    glClearColor(0.2, 0.2, 1, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-}
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int RECT_SIZE = 50;
+const int SPEED = 5;
 
 int main() {
-    glfwSetErrorCallback(errorCallback);
-    if (!glfwInit()) {
-        printf("glfwinit error\n");
-        exit(1);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
+                  << std::endl;
+        return 1;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    GLFWwindow *window = glfwCreateWindow(800, 600, "GLFW Window", NULL, NULL);
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2 Game Loop", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
-        printf("window creation error\n");
-        exit(1);
+        std::cerr << "Window could not be created! SDL_Error: "
+                  << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
     }
 
-    glfwMakeContextCurrent(window);
-    gladLoadGL();
-
-    glfwSetKeyCallback(window, keyCallback);
-
-    glfwSwapInterval(1);
-    double time = glfwGetTime();
-
-    while (!glfwWindowShouldClose(window)) {
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        glViewport(0, 0, width, height);
-
-        update();
-
-        draw();
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+    SDL_Renderer *renderer =
+        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Renderer could not be created! SDL_Error: "
+                  << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
     }
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    // position
+    SDL_Rect rect = {SCREEN_WIDTH / 2 - RECT_SIZE / 2,
+                     SCREEN_HEIGHT / 2 - RECT_SIZE / 2, RECT_SIZE, RECT_SIZE};
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        // handle events
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            if (event.type == SDL_KEYDOWN &&
+                event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+                running = false;
+            }
+        }
+
+        // Keyboard state
+        const Uint8 *keystate = SDL_GetKeyboardState(NULL);
+        if (keystate[SDL_SCANCODE_W])
+            rect.y -= SPEED;
+        if (keystate[SDL_SCANCODE_S])
+            rect.y += SPEED;
+        if (keystate[SDL_SCANCODE_A])
+            rect.x -= SPEED;
+        if (keystate[SDL_SCANCODE_D])
+            rect.x += SPEED;
+        if (keystate[SDL_SCANCODE_ESCAPE])
+            running = false;
+
+        // clear screen
+        SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+        SDL_RenderClear(renderer);
+
+        // draw rectangle
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+
+        // present renderer
+        SDL_RenderPresent(renderer);
+
+        // delay for frame rate control
+        SDL_Delay(16);
+    }
+
+    // cleanup
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }

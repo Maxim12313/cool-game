563b1982d5ca93bd1659037b96c0e2f9db43f63d
maxim kim <maximk@umich.edu>
2025-04-02 22:26:01 -0400

    restart. rebuild with ecs system

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,64 @@
-#include "../include/game.hpp"
+#include "../include/config.hpp"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_oldnames.h>
+#include <SDL3/SDL_render.h>
+#include <iostream>
+
+void setBackground(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+    SDL_RenderClear(renderer);
+}
+
+void renderScreen(SDL_Renderer *renderer) {
+    SDL_RenderPresent(renderer);
+    SDL_Delay(Config::fpsDelay);
+}
+
+void cleanup(SDL_Renderer *renderer, SDL_Window *window) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
 int main() {
-    gameLoop();
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
+                  << std::endl;
+        exit(1);
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Game Loop", Config::windowWidth,
+                                          Config::windowHeight, 0);
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: "
+                  << SDL_GetError() << std::endl;
+        SDL_Quit();
+        exit(1);
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        std::cerr << "Renderer could not be created! SDL_Error: "
+                  << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        exit(1);
+    }
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        setBackground(renderer);
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+            if (event.type == SDL_EVENT_KEY_DOWN &&
+                event.key.scancode == SDL_SCANCODE_ESCAPE) {
+                running = false;
+            }
+        }
+        renderScreen(renderer);
+    }
+    cleanup(renderer, window);
 }
